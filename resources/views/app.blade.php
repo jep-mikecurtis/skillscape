e8858c243d4eb50075fb1885d68aab96abca4b93
Michael Curtis <mcurtis@jepcapital.com>
2025-11-08 11:54:56 -0800

    Default dark theme

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,9 +35,13 @@
 
         <title inertia>Skillscape - Master Your Journey</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.ico" sizes="32x32">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <meta name="theme-color" content="#D4AF37">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
